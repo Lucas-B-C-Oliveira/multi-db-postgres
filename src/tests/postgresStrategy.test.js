b43cd7379a782cea76224da5cf9e7e6523cf6eb4
Lucas B C Oliveira <lucasbcoliveira189@protonmail.com>
2021-09-15 21:00:57 -0300

    Implemented Create Function

--- src/tests/postgresStrategy.test.js
+++ src/tests/postgresStrategy.test.js
@@ -3,11 +3,36 @@ const Postgres = require('./../db/strategies/postgres')
 const Context = require('./../db/strategies/base/contextStrategy')
 
 const context = new Context(new Postgres)
+const MOCK_HERO_REGISTER = {
+    name: 'Black Hawk',
+    power: "Magic Arrow"
+}
 
-describe('Postgres Strategy', () => {
-    // this.timeout(Infinity)
-    it('PostgresSQL Connection', async () => {
+
+describe('Postgres Strategy', function () {
+    this.timeout(Infinity)
+
+    this.beforeAll(async function () {
+        await context.connect()
+    })
+
+    it('PostgresSQL Connection', async function () {
         const result = await context.isConnected()
         assert.equal(result, true)
     })
+
+    it.only('Register', async function () {
+        const result = await context.create(MOCK_HERO_REGISTER)
+        delete result.id
+        assert.deepStrictEqual(result, MOCK_HERO_REGISTER)
+    })
 })
+
+
+
+
+
+
+
+
+
